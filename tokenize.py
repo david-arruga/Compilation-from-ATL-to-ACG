aed3492af74a9c245177e5a6e92374bf8c8fcb8c
David Arruga <151402314+daivd-arruga@users.noreply.github.com>
2025-03-05 23:40:57 +0100

    Create tokenize.py

---
+++ tokenize.py
@@ -0,0 +1,63 @@
+def tokenize(source):
+    tokens = []
+    cursor = 0
+    length = len(source)
+
+    keywords = {
+        "and": AND,
+        "or": OR,
+        "not": NOT,
+        "next": NEXT,
+        "until": UNTIL,
+        "release": RELEASE,
+        "globally": GLOBALLY,
+        "eventually": EVENTUALLY,
+        "implies": IMPLIES
+    }
+
+    inside_agents = False  
+
+    while cursor < length:
+        symbol = source[cursor]
+
+        if symbol in " \t\n":
+            pass  
+
+        elif symbol == "(":
+            tokens.append((LPAREN, symbol))
+        elif symbol == ")":
+            tokens.append((RPAREN, symbol))
+        elif symbol == "<":
+            tokens.append((LTRI, symbol))
+            inside_agents = True  
+        elif symbol == ">":
+            tokens.append((RTRI, symbol))
+            inside_agents = False  
+        elif symbol == ",":
+            tokens.append((COMMA, symbol))
+        elif symbol == "|":
+            tokens.append((OR, symbol))
+        elif symbol == "&":
+            tokens.append((AND, symbol))
+
+        elif symbol.isalpha():
+            buffer = symbol
+            cursor += 1
+            while cursor < length and source[cursor].isalnum():
+                buffer += source[cursor]
+                cursor += 1
+
+            if inside_agents:
+                tokens.append((AGENT_NAME, buffer))
+            else:
+                token_type = keywords.get(buffer, PROPOSITION)
+                tokens.append((token_type, buffer))
+
+            cursor -= 1  
+
+        else:
+            tokens.append((UNKNOWN, symbol))
+
+        cursor += 1
+
+    return tokens
