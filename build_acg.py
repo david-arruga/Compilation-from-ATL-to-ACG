77dcfaf337ba8e709dd3b291a92d97f0871bb7c2
David Arruga <151402314+daivd-arruga@users.noreply.github.com>
2025-03-13 21:14:00 +0100

    Update build_acg.py

--- build_acg.py
+++ build_acg.py
@@ -1,68 +1,13 @@
-def build_acg(formula):
-    acg = ACG()  
+def build_acg(transformed_ast):
+    
+    ap_set = extract_propositions(transformed_ast)
+    alphabet = set(frozenset(s) for s in chain.from_iterable(combinations(ap_set, r) for r in range(len(ap_set) + 1)))
+    acg = ACG()
+    acg.propositions = ap_set
+    acg.alphabet = alphabet 
+    closure = generate_closure(transformed_ast)
+    acg.states = closure  
+    acg.initial_state = transformed_ast  
+    generate_transitions(acg)
 
-    def collect_subformulas(node):
-
-        if node not in acg.states:
-            acg.add_state(node)  
-
-            if isinstance(node, Var):  
-                acg.add_proposition(node.name)  
-
-            for child in getattr(node, "__dict__", {}).values():
-                if isinstance(child, ParseNode):
-                    collect_subformulas(child)
-                elif isinstance(child, list):
-                    for item in child:
-                        if isinstance(item, ParseNode):
-                            collect_subformulas(item)
-
-    collect_subformulas(formula)
-    acg.add_initial_state(formula)  
-
-    for state in acg.states:
-        for sigma in acg.alphabet:  
-            
-            if isinstance(state, Var):  
-                if state.name in sigma:
-                    acg.add_transition(state, sigma, state, "epsilon")  # True 
-                else:
-                    acg.add_transition(state, sigma, state, "epsilon")  # False 
-            
-            elif isinstance(state, Not) and isinstance(state.sub, Var):  
-                negated_prop = state.sub.name
-                if negated_prop in sigma:
-                    acg.add_transition(state, sigma, state, "epsilon")  # False 
-                else:
-                    acg.add_transition(state, sigma, state, "epsilon")  # True 
-
-            elif isinstance(state, And):  
-                acg.add_transition(state, sigma, state.lhs, "epsilon")  
-                acg.add_transition(state, sigma, state.rhs, "epsilon")  
-            
-            elif isinstance(state, Or):  
-                acg.add_transition(state, sigma, state.lhs, "epsilon")  
-                acg.add_transition(state, sigma, state.rhs, "epsilon")  
-
-
-            elif isinstance(state, Modality) and isinstance(state.sub, Next):
-                next_state = state.sub.sub  
-                agents = frozenset(state.agents)  
-                acg.add_transition(state, sigma, next_state, "existential", agents)  
-
-            elif isinstance(state, Modality) and isinstance(state.sub, Globally):
-                phi = state.sub.sub  
-                agents = frozenset(state.agents)  
-                acg.add_transition(state, sigma, phi, "epsilon")
-                acg.add_transition(state, sigma, state, "existential", agents)
-
-            elif isinstance(state, Modality) and isinstance(state.sub, Until):
-                phi1 = state.sub.lhs  
-                phi2 = state.sub.rhs  
-                agents = frozenset(state.agents)  
-                acg.add_transition(state, sigma, phi2, "epsilon")
-                acg.add_transition(state, sigma, phi1, "epsilon")
-                acg.add_transition(state, sigma, state, "existential", agents)
-
-
-    return acg
+    return acg  
